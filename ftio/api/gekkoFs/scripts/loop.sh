350ea55fad636750495700dbc3abd39bd4b19bdc
Ahmad Tarraf <engr.ahmad.t@gmail.com>
2024-04-19 14:05:58 +0200

    JIT: added loop

---
+++ ftio/api/gekkoFs/scripts/loop.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+#------------------------------------------------------------------------------------------------------------
+# Settings
+#------------------------------------------------------------------------------------------------------------
+
+# Executable name
+EXECUTABLE="jit.sh"
+
+
+# Nodes
+nodes=(1 24 48 96 192 384 768 1536 3072 6144 12288 24576)
+
+
+#------------------------------------------------------------------------------------------------------------
+# load defaults
+#------------------------------------------------------------------------------------------------------------
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source ${SCRIPT_DIR}/default.sh
+
+
+
+#------------------------------------------------------------------------------------------------------------
+# Main
+#------------------------------------------------------------------------------------------------------------
+echo -e "${GREEN}\n--------------------   Loop   -------------------\n${BLACK}Executable name: ${BLUE}${EXECUTABLE}\n${BLACK}\n\n "
+echo -e "Enter a number great than 0 to run a single run.\nA number bellow 0 will launch a code specific for loop. "
+echo -e "Suggestions:  ${YELLOW}1 24 48 96 192 384 768 1536 3072 6144 12288 24576 ...${BLACK}"
+read -p "Enter number of process: " n
+
+#direct run
+if [[ $n -gt 0 ]]; then
+    ${SCRIPT_DIR}/default.sh 
+#loop over nodes
+else
+    for NODES in ${nodes[@]}; do
+		info
+		NODES=$n
+		MAX_TIME=$(find_time $NODES)
+        ${SCRIPT_DIR}/${EXECUTABLE} -n ${NODES}  -t ${MAX_TIME} &
+		progress
+    done
+fi
+
+echo -e "${GREEN}\n--------------------       done       -------------------\n${BLACK}"
