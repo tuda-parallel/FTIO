dcb6eb547632413ffd4c8f9eab4f94586a8ba46c
BESNARD Jean-Baptiste <jbbesnard@paratools.com>
2024-08-05 14:01:38 +0200

    Proxy: add read data from stdin

--- ftio/api/metric_proxy/parse_proxy.py
+++ ftio/api/metric_proxy/parse_proxy.py
@@ -1,5 +1,7 @@
 import numpy as np
 import json
+import sys
+
 from time import process_time
 from ftio.freq.helper import MyConsole
 
@@ -53,28 +55,9 @@ def extract(json_data, match, verbose=False):
     return b_out,t_out
 
 
-def parse_all(file_path:str,deriv_and_not_deriv:bool=True, exclude=None)-> dict:
-    """parses all metrics from proxy
-
-    Args:
-        file_path (str): pass to proxy JSON file
-        deriv_and_not_deriv (bool, optional): Removes the metrics in case a similar metrics, which start with deriv is presented. Defaults to True.
-        exclude (list,optional): list of metrics to exclude
-
-    Returns:
-        dict: parsed metrics with 2D numpy array
-    """
+def filter_deriv(json_data,deriv_and_not_deriv:bool=True, exclude=None):
     out = {}
     t = process_time()
-    try:
-        with open(file_path, 'r') as json_file:
-            json_data = json.load(json_file)
-    except FileNotFoundError:
-        print(f"Error: File '{file_path}' not found.")
-        return out
-    except json.JSONDecodeError:
-        print(f"Error: Unable to decode JSON from file '{file_path}'. Check if the file is valid JSON.")
-        return out
 
     metrics = json_data['metrics'].keys()
     # extract either derive or all but not all
@@ -94,14 +77,48 @@ def parse_all(file_path:str,deriv_and_not_deriv:bool=True, exclude=None)-> dict:
             b_out,t_out = extract(json_data,metric, False)
             out[metric]=[b_out,t_out]
 
-    CONSOLE.info(f"\n[green]Parsing time: {elapsed_time} s[/]")
+    #CONSOLE.info(f"\n[green]Parsing time: {elapsed_time} s[/]")
     return out
+
+
+def parse_all(file_path:str,deriv_and_not_deriv:bool=True, exclude=None)-> dict:
+    """parses all metrics from proxy
+
+    Args:
+        file_path (str): pass to proxy JSON file
+        deriv_and_not_deriv (bool, optional): Removes the metrics in case a similar metrics, which start with deriv is presented. Defaults to True.
+        exclude (list,optional): list of metrics to exclude
+
+    Returns:
+        dict: parsed metrics with 2D numpy array
+    """
+    try:
+        with open(file_path, 'r') as json_file:
+            json_data = json.load(json_file)
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return {}
+    except json.JSONDecodeError:
+        print(f"Error: Unable to decode JSON from file '{file_path}'. Check if the file is valid JSON.")
+        return {}
+
+    return filter_deriv(json_data,deriv_and_not_deriv,exclude)
+
+
+def load_proxy_trace_stdin(deriv_and_not_deriv:bool=True, exclude=None):
+    try:
+    # Read JSON from stdin
+        data = json.load(sys.stdin)
+    except json.JSONDecodeError as e:
+        print(f"Error parsing JSON: {e}")
+    else:
+        return filter_deriv(data,deriv_and_not_deriv,exclude)
 
 
 def clean_metrics(metrics):
     deriv_metrics =  [metric for metric in metrics if metric.startswith("deriv")]
     non_deriv_metrics = [metric for metric in metrics if not metric.startswith("deriv")]
     cleaned_metrics = [metric for metric in metrics if not (metric in non_deriv_metrics and "deriv__" + metric in deriv_metrics)]
-    CONSOLE.info(f"[green]Metrics reduced from {len(metrics)} to {len(cleaned_metrics)}[/]")
+    #CONSOLE.info(f"[green]Metrics reduced from {len(metrics)} to {len(cleaned_metrics)}[/]")
     return cleaned_metrics
 
