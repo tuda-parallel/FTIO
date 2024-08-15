55469ec46cb379851335a7068ec8e479196a4ae8
Ahmad Tarraf <ahmad.tarraf@tu-darmstadt.de>
2024-08-15 17:23:35 +0200

    JIT: updated plotting script

--- ftio/api/gekkoFs/jit/jit_plot.py
+++ ftio/api/gekkoFs/jit/jit_plot.py
@@ -6,49 +6,50 @@ from ftio.plot.helper import format_plot
 def main():
     # Sample data for the stacked plot
     categories = ['JIT', 'JIT no FTIO', 'Pure']
-
-    app_time = [207.3, 181.44, 181.56]
-    stage_out_time = [3.95, 17.68, 0]
-    stage_in_time = [0.72, 0.75, 0]
-    title = "Nek5000 nodes 2 steps 100 write every 10 steps"
-    plot_res(categories, app_time,stage_out_time, stage_in_time, title)
-
-    app_time = [90.11, 84.81, 103]
-    stage_out_time = [2.26, 61.0, 0]
-    stage_in_time = [1.11, 1.11, 0]
-    title = "Nek5000 nodes 3 steps 100 write every 10 steps"
-    plot_res(categories, app_time,stage_out_time, stage_in_time, title)
-
-
-def plot_res(categories, app_time,stage_out_time, stage_in_time, title):
-    # Creating a stacked bar chart
+    # add nodes if need 
+    nodes=[]
     fig = go.Figure()
+    title = "Nek5000 with 100 steps and checkpointing every 10 steps"
 
-    fig.add_trace(go.Bar(
-        x=categories,
-        y=app_time,
-        name='APP Time'
-    ))
 
-    fig.add_trace(go.Bar(
-        x=categories,
-        y=stage_out_time,
-        name='Stage out'
-    ))
+    # run with 2 nodes [jit|jit_no_ftio|pure]
+    ################ Edit area ############################
+    app = [207.3, 181.44, 181.56]
+    stage_out = [3.95,  17.68, 0]
+    stage_in =  [0.72,  0.75, 0]
+    nodes.append('node 2')
 
-    fig.add_trace(go.Bar(
-        x=categories,
-        y=stage_in_time,
-        name='Stage in'
-    ))
+    # run with 3 nodes
+    tmp_app = [90.11, 84.81, 103]
+    tmp_stage_out = [2.26, 61.0, 0]
+    tmp_stage_in = [1.11, 1.11, 0]
+    nodes.append('node 3')
+    app,stage_out,stage_in = add_experiment(app,stage_out,stage_in,tmp_app,tmp_stage_out,tmp_stage_in)
+
+
+    tmp_app = [70.53, 72.71, 80.21]
+    tmp_stage_out = [1.891,9.430, 0]
+    tmp_stage_in = [1.149, 1.145, 0]
+    nodes.append('node 4')
+    app,stage_out,stage_in = add_experiment(app,stage_out,stage_in,tmp_app,tmp_stage_out,tmp_stage_in)
+
+
+    ##################################
+    repeated_strings = [s for s in categories for _ in nodes]
+    repeated_numbers = nodes * len(categories)
+    categories = [repeated_strings, repeated_numbers]
+
+    fig.add_bar(x=categories, y=app, name="App")
+    fig.add_bar(x=categories, y=stage_out, name="Stage out")
+    fig.add_bar(x=categories, y=stage_out, name="Stage out")
 
     # Update the layout to stack the bars
     fig.update_layout(
-        barmode='stack',
         yaxis_title="Time (s)",
         xaxis_title=f"Runs",
         showlegend=True,
-        title=title
+        title=title,
+        barmode="relative"
         )
 
     format_plot(fig)
@@ -56,6 +57,27 @@ def plot_res(categories, app_time,stage_out_time, stage_in_time, title):
     # Display the plot
     fig.show()
 
+def add_experiment(app,stage_out,stage_in,tmp_app,tmp_stage_out,tmp_stage_in):
+    app = add_mode(app,tmp_app)
+    stage_out = add_mode(stage_out,tmp_stage_out)
+    stage_in = add_mode(stage_in,tmp_stage_in)
+    return app,stage_out,stage_in
+
+
+def add_mode(list1, list2):
+    # Create a new list to store the result
+    result = []
+    
+
+    
+    # Iterate over the range of the lists' lengths
+    for i in range(len(list2)):
+        # Add the current element from each list    
+        for j in range(int(len(list1)/3)):
+            result.append(list1[i+j])
+        result.append(list2[i])
+    
+    return result
 
 if __name__ == "__main__":
     main()
