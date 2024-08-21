b881e11ba714957d6d7e310b8d36f3c86d7419be
Ahmad Tarraf <ahmad.tarraf@tu-darmstadt.de>
2024-08-21 12:27:26 +0200

    cleaned code

--- ftio/api/trace_analysis/trace_analysis.py
+++ ftio/api/trace_analysis/trace_analysis.py
@@ -8,10 +8,12 @@ from rich.panel import Panel
 import numpy as np
 import pandas as pd
 from ftio.api.trace_analysis.trace_ftio_v2 import main as trace_ftio
+
 console = Console()
 
+
 def main(argv=sys.argv[1:]) -> None:
-    start_time = time.time() 
+    start_time = time.time()
     pattern = "_signal_plafrim.csv"
     df = pd.DataFrame()
 
@@ -45,17 +47,20 @@ def main(argv=sys.argv[1:]) -> None:
 
             # Iterate over each csv file
             for file_path in csv_files:
-                    # Display the file being processed
-                progress.console.print(f"Processing ({csv_files.index(file_path)}/{len(csv_files)}): {file_path}")
+                # Display the file being processed
+                progress.console.print(
+                    f"Processing ({csv_files.index(file_path)}/{len(csv_files)}): {file_path}"
+                )
 
                 # Run the trace_ftio function
-                
-                
-                res = trace_ftio([file_path]+argv,False)
-                
+
+                res = trace_ftio([file_path] + argv, False)
+
                 # Create the new file name by replacing the pattern
                 base_name = os.path.basename(file_path)
-                new_file_name = base_name.replace("_signal_plafrim.csv", "_freq_plafrim.json")
+                new_file_name = base_name.replace(
+                    "_signal_plafrim.csv", "_freq_plafrim.json"
+                )
                 new_file_path = os.path.join(os.path.dirname(file_path), new_file_name)
 
                 # Write the content to the new file
@@ -77,8 +82,9 @@ def main(argv=sys.argv[1:]) -> None:
 
                 # Update the progress bar
                 progress.advance(task)
-                
+
         progress.console.print("[bold green]All files processed successfully![/]")
+        console.print(f"[blue]FTIO total time: {time.time()  - start_time:.4f} seconds[/]")
         df.to_csv("ftio.csv", index=False)
         statisitcs(df)
     except KeyboardInterrupt:
@@ -86,6 +92,7 @@ def main(argv=sys.argv[1:]) -> None:
         statisitcs(df)
         sys.exit()
     console.print(f"[blue]Execution time: {time.time()  - start_time:.4f} seconds[/]")
+
 
 def convert_dict(data):
     """Convert NumPy arrays and sets to lists in the dictionary."""
@@ -119,71 +126,78 @@ def flatten_dict(d):
     return flat
 
 
-
 def statisitcs(df):
     print(df)
     dom_df = reduce_to_max_conf(df)
     prefixes = ["read", "write", "both"]
     color = ["purple4", "gold3", "deep_sky_blue1"]
-    
+
     for prefix in prefixes:
         s = ""
-        s +=  periodic_apps(df,prefix)
-        s +=  min_max_mean(dom_df,prefix, "conf")
-        s +=  min_max_mean(dom_df,prefix,"dominant_freq","Hz")
-        s +=  time_app(df,prefix)
-        console.print(Panel.fit(s, title=prefix.capitalize(), border_style=color[prefixes.index(prefix)], title_align="left"))
+        s += periodic_apps(df, prefix)
+        s += min_max_mean(dom_df, prefix, "conf")
+        s += min_max_mean(dom_df, prefix, "dominant_freq", "Hz")
+        s += time_app(df, prefix)
+        console.print(
+            Panel.fit(
+                s,
+                title=prefix.capitalize(),
+                border_style=color[prefixes.index(prefix)],
+                title_align="left",
+            )
+        )
     # print(dom_df)
-    
 
 
-def periodic_apps(df,prefix) -> str:
-    all = len(df[f'{prefix}_dominant_freq'])
+def periodic_apps(df, prefix) -> str:
+    all = len(df[f"{prefix}_dominant_freq"])
     # n = df[f'{prefix}_dominant_freq'].apply(lambda x: len(x)>0).sum()
-    n = df[f'{prefix}_dominant_freq'].apply(lambda x: not np.isnan(x)).sum()
+    n = df[f"{prefix}_dominant_freq"].apply(lambda x: not np.isnan(x)).sum()
     # out = f"[blue]Periodic {prefix.capitalize()}: {n:.2f}/{all:.2f} ({n/all*100:.2f}%)[/]"
     out = f"[blue]Periodic I/O: {n:.2f}/{all:.2f} ({n/all*100:.2f}%)[/]\n\n"
     return out
-    
 
-def min_max_mean(df:pd.DataFrame,prefix, suffix="conf",unit="%",title="") -> str:
+
+def min_max_mean(df: pd.DataFrame, prefix, suffix="conf", unit="%", title="") -> str:
     if not title:
         title = suffix.capitalize()
     conf_col = f"{prefix}_{suffix}"
     min = np.min(df[conf_col])
     max = np.max(df[conf_col])
-    mean= np.mean(df[conf_col])
-    median= np.median(df[conf_col])
-    nanmean= np.nanmean(df[conf_col])
-    nanmedian= np.nanmedian(df[conf_col])
+    mean = np.mean(df[conf_col])
+    median = np.median(df[conf_col])
+    nanmean = np.nanmean(df[conf_col])
+    nanmedian = np.nanmedian(df[conf_col])
     scale = 100 if "conf" in suffix else 1
     # out = f"[green]{prefix.capitalize()} {title}:\n - range: [{min*scale:.2f},{max*scale:.2f}] {unit}\n - mean: {mean*scale:.2f} {unit}\n - nanmean: {nanmean*scale:.2f} {unit}\n - median: {median*scale:.2f} {unit}\n - nanmedian: {nanmedian*scale:.2f} {unit}\n[/]"
     out = f"[gray][green]{title}[/]:\n - range: [{min*scale:.2f},{max*scale:.2f}] {unit}\n - mean: {mean*scale:.2f} {unit}\n - nanmean: {nanmean*scale:.2f} {unit}\n - median: {median*scale:.2f} {unit}\n - nanmedian: {nanmedian*scale:.2f} {unit}\n\n[/]"
-    return  out
-
-def time_app(df,prefix):
-    df[f"{prefix}_time"] = df[f"{prefix}_t_end"]- df[f"{prefix}_t_start"]
-    out = min_max_mean(df,prefix,"time","sec","I/O Time")
     return out
 
-    
-def reduce_to_max_conf(df:pd.DataFrame) -> pd.DataFrame:
+
+def time_app(df, prefix):
+    df[f"{prefix}_time"] = df[f"{prefix}_t_end"] - df[f"{prefix}_t_start"]
+    out = min_max_mean(df, prefix, "time", "sec", "I/O Time")
+    return out
+
+
+def reduce_to_max_conf(df: pd.DataFrame) -> pd.DataFrame:
     prefixes = ["read", "write", "both"]
     # Iterate over each row
     for i, row in df.iterrows():
-        for prefix in prefixes:    
+        for prefix in prefixes:
             conf_col = f"{prefix}_conf"
             freq_col = f"{prefix}_dominant_freq"
             freq = np.nan
             conf = np.nan
-            if isinstance(row[conf_col], list) and len(row[conf_col]) > 0:  
+            if isinstance(row[conf_col], list) and len(row[conf_col]) > 0:
                 dominant_index = np.argmax(row[conf_col])
                 freq = row[freq_col][dominant_index]
                 conf = row[conf_col][dominant_index]
             df.at[i, conf_col] = conf
             df.at[i, freq_col] = freq
-        
+
     return df
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
