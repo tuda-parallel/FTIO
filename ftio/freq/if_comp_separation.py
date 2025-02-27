2ddaff3462c83eec5dee807b1ea95bbd06061389
Josefine Zeller <josefine_zeller@web.de>
2025-02-27 14:36:11 +0100

    [ftio/freq/if_comp/sep]: binary image: use n most prominent peaks, extend livetime of peak

--- ftio/freq/if_comp_separation.py
+++ ftio/freq/if_comp_separation.py
@@ -12,7 +12,7 @@ component_linking:
 import cv2
 import numpy as np
 from scipy.signal import find_peaks, peak_prominences
-from ftio.freq.anomaly_detection import z_score
+from ftio.freq.anomaly_detection import z_score, remove_harmonics
 
 """
 Rankine, L., Mesbah, M., & Boashash, B. (2007).
@@ -27,11 +27,39 @@ def binary_image(Zxx):
     for i in range(0,rows):
         freqs = np.abs(Zxx[i])
         peaks = find_peaks(freqs)
+
         prom = peak_prominences(freqs, peaks[0])[0]
 
         if(prom.size > 0):
             for ind in range(0,len(prom)):
                 if prom[ind] > 0.01:
+                    _ind = peaks[0][ind]
+                    bin_im[i][_ind] = 255
+
+    return bin_im
+
+def binary_image_nprom(Zxx):
+    bin_im = np.zeros_like(Zxx, dtype="uint8")
+    rows = np.shape(Zxx)[0]
+
+    for i in range(0,rows):
+        freqs = np.abs(Zxx[i])
+        peaks = find_peaks(freqs)
+
+        prom = peak_prominences(freqs, peaks[0])[0]
+        prom_sorted = np.argsort(prom)
+
+        prom_filtered = []
+        if (len(peaks[0]) > 3):
+            prom_filtered.append(prom[prom_sorted[-1]])
+            prom_filtered.append(prom[prom_sorted[-2]])
+            prom_filtered.append(prom[prom_sorted[-3]])
+        else:
+            prom_filtered = prom
+
+        if(len(prom_filtered) > 0):
+            for ind in range(0,len(prom_filtered)):
+                if prom_filtered[ind] > 0.1:
                     _ind = peaks[0][ind]
                     bin_im[i][_ind] = 255
 
@@ -49,6 +77,39 @@ def binary_image_zscore(Zxx, freq, args):
 
         for ind in indices:
             bin_im[i][ind] = 255
+
+    return bin_im
+
+def binary_image_zscore_extended(Zxx, freq, args):
+    bin_im = np.zeros_like(Zxx, dtype="uint8")
+    rows = np.shape(Zxx)[0]
+
+    zscore_freq = []
+
+    for i in range(0,rows):
+        freqs = np.abs(Zxx[i])
+        peaks_ = find_peaks(freqs)
+
+        n = len(freqs)
+        freq_arr = freq * np.arange(0, n) / n
+        indices = z_score(freqs, freq_arr, args)[0]
+
+        peaks = remove_harmonics(freq_arr, freq_arr, peaks_[0])[0]
+        prom = peak_prominences(freqs, peaks)[0]
+
+        for ind in indices:
+            if not ind in zscore_freq:
+                zscore_freq.append(ind)
+                zscore_freq.append(ind-1)
+                zscore_freq.append(ind+1)
+
+        if(prom.size > 0):
+            for ind in range(0,len(prom)):
+                if prom[ind] > 0.01:
+                    #_ind = peaks[0][ind]
+                    _ind = peaks[ind]
+                    if _ind in zscore_freq:
+                        bin_im[i][_ind] = 255
 
     return bin_im
 
@@ -85,6 +146,11 @@ def component_linking(image):
 
         comp = time, freqs
         result.append(comp)
+
+    #filename = "cv2_image.jpg"
+    #cv2.imwrite(filename, frame)
+    #filename = "cv2_filtered.jpg"
+    #cv2.imwrite(filename, output)
 
     cv2.imshow("Image", frame)
     cv2.imshow("Filtered Components", output)
