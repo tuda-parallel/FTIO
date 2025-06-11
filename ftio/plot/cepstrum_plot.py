c35be233d4ce1dfb57caca43c84db770d93b38eb
AntonBeasis <anton@holderied.de>
2025-06-11 12:24:23 +0200

    Reapply stashed changes after rebase

--- ftio/plot/cepstrum_plot.py
+++ ftio/plot/cepstrum_plot.py
@@ -1,12 +1,13 @@
-"""Outlier plot function
-"""
+"""Outlier plot function"""
 
 from __future__ import annotations
-import numpy as np
-import plotly.graph_objects as go
-import plotly.express as px
+
 import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
 from sklearn.inspection import DecisionBoundaryDisplay
+
 from ftio.freq.freq_html import create_html
 from ftio.plot.helper import format_plot
 from ftio.plot.spectrum import plot_both_spectrums
@@ -37,14 +38,14 @@ def plot_cepstrum(
     """
     colorscale = [(0, "rgb(0,50,150)"), (0.5, "rgb(150,50,150)"), (1, "rgb(255,50,0)")]
     labels = [(f"cluster {i}") if (i >= 0) else "outliers" for i in dominant_index]
-    #prepare figures
+    # prepare figures
     figs = []
     for i in np.arange(0, 4):
         f = go.Figure()
         f = format_plot(f)
         figs.append(f)
 
-    #prepare symbols
+    # prepare symbols
     if dominant_index.size > 0 and dominant_index.max() < 20:
         symbol = dominant_index.copy()
         symbol[symbol >= 5] = symbol[symbol >= 5] + 1
@@ -54,7 +55,7 @@ def plot_cepstrum(
         symbol = np.ones(dominant_index.size)
     labels = np.array(labels)
 
-    if d.size  == 0 and len(freq_arr) != 0:
+    if d.size == 0 and len(freq_arr) != 0:
         d = np.vstack(
             (
                 freq_arr[indecies] / freq_arr[indecies].max(),
@@ -66,11 +67,17 @@ def plot_cepstrum(
     else:
         pass
 
-    
-    all_colors = [px.colors.qualitative.Alphabet[0], px.colors.qualitative.Plotly[0]] + px.colors.qualitative.Plotly[2:]
-    all_colors = np.array(all_colors + px.colors.qualitative.Alphabet + px.colors.qualitative.Dark24 + [px.colors.qualitative.Plotly[1]])
+    all_colors = [
+        px.colors.qualitative.Alphabet[0],
+        px.colors.qualitative.Plotly[0],
+    ] + px.colors.qualitative.Plotly[2:]
+    all_colors = np.array(
+        all_colors
+        + px.colors.qualitative.Alphabet
+        + px.colors.qualitative.Dark24
+        + [px.colors.qualitative.Plotly[1]]
+    )
 
-    
     counter = -1
     spec_figs, plt_names = plot_both_spectrums(args, freq_arr, amp, full=False)
     for trace in list(spec_figs.select_traces()):
@@ -84,11 +91,11 @@ def plot_cepstrum(
     for i in np.arange(0, 2):
         figs[i].update_layout(coloraxis={"colorscale": colorscale})
 
-    power_spectrum = amp*amp
+    power_spectrum = amp * amp
     powerlog = np.log(power_spectrum + 1e-10)
-    #powerlog -= np.mean(powerlog)
-    #print (len(powerlog))
-    freq_arr_qf = freq_arr * ((len(freq_arr)/20)/5)
+    # powerlog -= np.mean(powerlog)
+    # print (len(powerlog))
+    freq_arr_qf = freq_arr * ((len(freq_arr) / 20) / 5)
     cepstrum = np.abs(np.fft.fft(powerlog).real)
 
     spec_figs, plt_names = plot_both_spectrums(args, freq_arr_qf, cepstrum, full=False)
@@ -102,7 +109,6 @@ def plot_cepstrum(
 
     for i in np.arange(2, 4):
         figs[i].update_layout(coloraxis={"colorscale": colorscale})
-
 
     y_title = "Power" if args.psd else "Amplitude"
     figs[0].update_xaxes(title_text="Frequency (Hz)")
@@ -132,4 +138,3 @@ def plot_decision_boundaries(model, d, conf):
     plt.axis("square")
     plt.legend(labels=["outliers", "inliers"], title="true class")
     plt.show()
-
