286435ac036028f1639882ccd6b1284038026696
AntonBeasis <anton@holderied.de>
2025-07-31 13:18:07 +0200

    fixed cepstrum plot

--- ftio/plot/cepstrum_plot.py
+++ ftio/plot/cepstrum_plot.py
@@ -21,10 +21,6 @@ def plot_cepstrum(
     freq_arr: np.ndarray,
     amp: np.ndarray,
     indecies: np.ndarray,
-    conf: np.ndarray,
-    dominant_index: np.ndarray,
-    d: np.ndarray = np.array([]),
-    eps: float = 0,
 ) -> None:
     """Plots outliers for DB scan
 
@@ -37,35 +33,12 @@ def plot_cepstrum(
         d (np.ndarray, optional): _description_. Defaults to np.array([]).
     """
     colorscale = [(0, "rgb(0,50,150)"), (0.5, "rgb(150,50,150)"), (1, "rgb(255,50,0)")]
-    labels = [(f"cluster {i}") if (i >= 0) else "outliers" for i in dominant_index]
     # prepare figures
     figs = []
     for i in np.arange(0, 4):
         f = go.Figure()
         f = format_plot(f)
         figs.append(f)
-
-    # prepare symbols
-    if dominant_index.size > 0 and dominant_index.max() < 20:
-        symbol = dominant_index.copy()
-        symbol[symbol >= 5] = symbol[symbol >= 5] + 1
-        symbol[symbol == -1] = 5  # x marker
-        symbol = symbol - 1
-    else:
-        symbol = np.ones(dominant_index.size)
-    labels = np.array(labels)
-
-    if d.size == 0 and len(freq_arr) != 0:
-        d = np.vstack(
-            (
-                freq_arr[indecies] / freq_arr[indecies].max(),
-                amp[indecies] / amp[indecies].sum(),
-            )
-        ).T
-    elif len(freq_arr) == 0:
-        return
-    else:
-        pass
 
     all_colors = [
         px.colors.qualitative.Alphabet[0],
@@ -93,8 +66,6 @@ def plot_cepstrum(
 
     power_spectrum = amp * amp
     powerlog = np.log(power_spectrum + 1e-10)
-    # powerlog -= np.mean(powerlog)
-    # print (len(powerlog))
     freq_arr_qf = freq_arr * ((len(freq_arr) / 20) / 5)
     cepstrum = np.abs(np.fft.fft(powerlog).real)
 
@@ -124,17 +95,3 @@ def plot_cepstrum(
         fig.update_layout(width=1300, height=400)
     configuration = {"toImageButtonOptions": {"format": "png", "scale": 4}}
     create_html(figs, args.render, configuration, "cepstrum")
-
-
-def plot_decision_boundaries(model, d, conf):
-    disp = DecisionBoundaryDisplay.from_estimator(
-        model,
-        d,
-        response_method="decision_function",
-        alpha=0.5,
-    )
-    disp.ax_.scatter(d[:, 0], d[:, 1], c=conf, s=20, edgecolor="k")
-    disp.ax_.set_title("Binary decision boundary \nof IsolationForest")
-    plt.axis("square")
-    plt.legend(labels=["outliers", "inliers"], title="true class")
-    plt.show()
